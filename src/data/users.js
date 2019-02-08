41a77b487b18562c3fd2b31a4b22e570261e7392
Ovik <ovikdevil@gmail.com>
2019-02-08 15:19:11 +0200

    updated inital data

--- src/data/users.js
+++ src/data/users.js
@@ -1,16 +1,83 @@
 const Users = [
-        {
-                id: 'id1',
-                first_name: 'Ovik',
-                last_name: 'Khahcikyan',
-                position: 'Front End Developer'
-        },
-        {
-                id: 'id2',
-                first_name: 'Osma',
-                last_name: 'Ahvenlampi',
-                position: 'Product Manager'
-        }
-];
+    {
+        id: 'id1',
+        first_name: 'Jouni',
+        last_name: 'Lounasmaa',
+        position: 'Chief Financial Officer',
+    },
+    {
+        id: 'id2',
+        first_name: 'Osma',
+        last_name: 'Ahvenlampi',
+        position: 'Product Manager',
+    },
+    {
+        id: 'id3',
+        first_name: 'Pekka',
+        last_name: 'Möttö',
+        position: 'Chief Executive Officer',
+    },
+    {
+        id: 'id4',
+        first_name: 'Pekka',
+        last_name: 'Niskanen',
+        position: 'Chief Operating Officer',
+    },
 
-module.exports = Users;
+    {
+        id: 'id5',
+        first_name: 'Johanna',
+        last_name: 'Taskinen',
+        position: '​Chief Experience Officer',
+    },
+    {
+        id: 'id6',
+        first_name: 'Paavo',
+        last_name: 'Moilanen',
+        position: 'Chief Technology Officer',
+    },
+    {
+        id: 'id7',
+        first_name: 'Hannu',
+        last_name: 'Tapanila',
+        position: 'Software Developer',
+    },
+    {
+        id: 'id8',
+        first_name: 'Tapani',
+        last_name: 'Moilanen',
+        position: 'Chief Backend Architect',
+    },
+    {
+        id: 'id9',
+        first_name: 'Victor',
+        last_name: 'Pavlushkov',
+        position: 'Software Developer',
+    },
+    {
+        id: 'id10',
+        first_name: 'Ville',
+        last_name: 'Ville',
+        position: 'Software Developer',
+    },
+    {
+        id: 'id11',
+        first_name: 'Tommi',
+        last_name: 'Vainikainen',
+        position: 'Software Developer',
+    },
+    {
+        id: 'id12',
+        first_name: 'Olavi',
+        last_name: 'Mustanoja',
+        position: 'Software Developer',
+    },
+    {
+        id: 'id13',
+        first_name: 'Ovik',
+        last_name: 'Khachikyan',
+        position: 'Software Developer',
+    },
+]
+
+module.exports = Users
